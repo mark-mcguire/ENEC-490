4f772487b6c07cbd8598cfd54ae311736ee241ef
mark-mcguire <30840718+mark-mcguire@users.noreply.github.com>
2017-08-30 15:51:32 -0400

    Update Lecture3MarkMcGuire.m

--- Lecture3MarkMcGuire.m
+++ Lecture3MarkMcGuire.m
@@ -1,1 +1,15 @@
-h
+s=xlsread('HenryHub.xls','Data 1', 'B136:B243');
+
+x=monthly_stats(s);
+
+jan = x(1,2)*randn(1000,1) + x(1,1);
+april = x(4,2)*randn(1000,1)+x(4,1);
+
+figure;
+edges = 28:.5:47;
+histogram(april,edges);
+hold on;
+histogram(jan,edges);
+xlabel('Natural Gas Price ($/MMBtu)');
+ylabel('Frequency');
+legend('April','January');
