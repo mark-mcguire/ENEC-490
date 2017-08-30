6558e75f74f92a7e0e00e82835af255852ad0d74
Jordan Kern <jdkern@live.unc.edu>
2017-08-30 16:00:08 -0400

    Update monthly_statsMarkMcGuire.m

--- monthly_statsMarkMcGuire.m
+++ monthly_statsMarkMcGuire.m
@@ -4,7 +4,7 @@ function [ m ] = monthly_stats( vector )
 m=zeros(12,2);
 
 for i=1:12
-    m(i,1)=mean(sum(vector(i:12:end,:)));
+    m(i,1)=mean((vector(i:12:end,:)));
     m(i,2)=std((vector(i:12:end,:)));
 end
 
