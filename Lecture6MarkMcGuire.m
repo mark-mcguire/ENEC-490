6ab71d7f66bdbb3427e5437bfa945b2555b4b67c
mark-mcguire <30840718+mark-mcguire@users.noreply.github.com>
2017-09-07 20:28:44 -0400

    Create Lecture6MarkMcGuire.m

---
+++ Lecture6MarkMcGuire.m
@@ -0,0 +1,86 @@
+%Lecture 6
+
+temps=csvread('tempdata.csv');
+temps=temps(:,2);
+%Read electricity demand data
+data_2014 = csvread('hourly-day-ahead-bid-data-2014.csv',5,1);
+vector = mat2vec(data_2014);
+peak = zeros(365,1);
+
+for i=1:365
+    peak(i) = max(data_2014(i,:));
+end
+
+peak=peak/1000;
+
+% forms 2-column matrix
+combined = [temps peak];
+
+% clusters for each row
+IDX = kmeans(combined,3);
+
+% forms 3-column matrix
+clustered_data = [combined IDX];
+
+% sorts 
+sorted_data = sortrows(clustered_data,3);
+
+% find indices of cluster 1
+ONE = clustered_data(clustered_data(:,3)==1,1:2);
+
+% find indices of cluster 2
+TWO = clustered_data(clustered_data(:,3)==2,1:2);
+
+% find indices of cluster 3
+THREE = clustered_data(clustered_data(:,3)==3,1:2);
+
+figure; 
+hold on;
+scatter(ONE(:,1),ONE(:,2),'r'); 
+scatter(TWO(:,1),TWO(:,2) ,'b');
+scatter(THREE(:,1),THREE(:,2) ,'g');
+xlabel('Average Temperature (degrees F)','FontSize',14);
+ylabel('Peak Electricity Demand (MWh)','FontSize',14);
+legend('Cluster 1', 'Cluster 2', 'Cluster 3');
+hold off;
+
+hourlydemand_jan=zeros(24,1);
+
+for i=1:24
+    hourlydemand_jan(i)=mean(data_2014(1:31,i));
+end
+
+hourlydemand_june=zeros(24,1);
+
+for i=1:24
+    hourlydemand_june(i)=mean(data_2014(182:212,i));
+end
+
+figure;
+hold on;
+plot(hourlydemand_jan);
+plot(hourlydemand_june);
+xlabel('Hour of Day', 'FontSize', 14);
+ylabel('Average Demand (MWh)','FontSize',14);
+legend('January','June');
+hold off;
+
+vec=1:1:7;
+x = [repmat(vec(1:7),52,1)];
+year=mat2vec(x);
+onerow=[1];
+weekdays=horzcat(year',onerow);
+weekdays=weekdays';
+a=[weekdays peak];
+
+p=sortrows(a,1);
+
+figure;
+hold on;
+boxplot(p(1:53,2));
+boxplot(p(54:105,2));
+boxplot(p(106:157,2));
+boxplot(p(158:209));
+boxplot(p(210:261,2));
+boxplot(p(262:313,2));
+boxplot(p(314:365,2));
